8d27ed6a9e7fe6a0a15333748dffad6d1729d479
Eduardo <edu.c.gomes.04@gmail.com>
2024-04-11 18:24:37 -0300

    Versão da aula do dia 10

---
+++ app/src/main/java/com/example/greenconnect/splash_screen.kt
@@ -0,0 +1,11 @@
+package com.example.greenconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
---
+++ app/src/main/java/com/example/greenconnect/tela_login.kt
@@ -0,0 +1,11 @@
+package com.example.greenconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_login)
+    }
+}
